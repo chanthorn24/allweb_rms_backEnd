5e7cf5478ac31747339049b440a3a4f5a989f526
SopheaYuos <yuossophea2000@gmail.com>
2022-08-18 15:13:59 +0700

    updated family and relatinship

--- src/Controller/Employee/EmpFamiliesController.php
+++ src/Controller/Employee/EmpFamiliesController.php
@@ -2,20 +2,113 @@
 
 namespace App\Controller\Employee;
 
+use App\Entity\EmpFamilies;
+use Doctrine\ORM\EntityManagerInterface;
+use RuntimeException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\param;
 
+/**
+ * @Route("employee/family")
+ */
 class EmpFamiliesController extends AbstractController
 {
-    /**
-     * @Route("/employee/emp/families", name="app_employee_emp_families")
-     */
-    public function index(): JsonResponse
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/Employee/EmpFamiliesController.php',
-        ]);
+        $this->em = $entityManager;
     }
+
+    /**
+     * @Route("/", name="get_family", methods="GET")
+     */
+    public function getAll(): JsonResponse
+    {
+        try {
+            $family = $this->em->getRepository(EmpFamilies::class)->findAll();
+            if (!$family) {
+                throw new RuntimeException("No data is found");
+            }
+            foreach ($family as $fam) {
+                $res[] = [
+                    "id" => $fam->getId(),
+                    "name" => $fam->getName(),
+                    "phone" => $fam->getPhone()
+                ];
+            }
+            return $this->json(array("success" => true, "message" => $res), 200);
+        } catch (\Exception $err) {
+            return $this->json(array("success" => false, "message" => $err->getMessage()), 400);
+        }
+    }
+
+    /**
+     * @Route("/create", name="create_family", methods="POST")
+     */
+    public function create(Request $request): JsonResponse
+    {
+        $param = json_decode($request->getContent(), true);
+        try {
+            $families = new EmpFamilies();
+            $families->setName($param['name']);
+            $families->setPhone($param['phone']);
+            $families->setIsDelete(false);
+
+            $this->em->persist($families);
+            $this->em->flush($families);
+
+            return $this->json(array("success" => true, "message" => "Created successfully"), 200);
+        } catch (\Exception $err) {
+            return $this->json(array("success" => false, "message" => $err->getMessage()), 400);
+        }
+
+    }
+
+    /**
+     * @Route("/update/{id}", name="update_family", methods="POST")
+     */
+    public function update(Request $request, $id): JsonResponse
+    {
+        $param = json_decode($request->getContent(), true);
+        try {
+            $family = $this->em->getRepository(EmpFamilies::class)->find($id);
+            if (!$family) {
+                throw new RuntimeException("No data is found");
+            }
+            if (isset($param['name']) && isset($param['phone'])) {
+                $family->setName($param['name']);
+                $family->setPhone($param['phone']);
+            }
+
+            $this->em->flush($family);
+
+            return $this->json(array("success" => true, "message" => "Updated successfully"), 200);
+        } catch (\Exception $err) {
+            return $this->json(array("success" => false, "message" => $err->getMessage()), 400);
+        }
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete_family", methods="DELETE")
+     */
+    public function delete($id): JsonResponse
+    {
+        try {
+            $family = $this->em->getRepository(EmpFamilies::class)->find($id);
+            if (!$family) {
+                throw new RuntimeException('No data is found');
+            }
+            $family->setIsDelete(true);
+            $this->em->flush($family);
+
+            return $this->json(array("success" => true, "message" => "Deleted successfully"), 200);
+        } catch (\Exception $err) {
+            return $this->json(array("success" => false, "message" => $err->getMessage()), 400);
+        }
+    }
+
 }
--- var/cache/dev/url_matching_routes.php
+++ var/cache/dev/url_matching_routes.php
@@ -18,10 +18,12 @@ return [
         '/employee/bank/create' => [[['_route' => 'create_banks', '_controller' => 'App\\Controller\\Employee\\BanksController::create'], null, ['POST' => 0], null, false, false, null]],
         '/department' => [[['_route' => 'all_department', '_controller' => 'App\\Controller\\Employee\\EmpDepartmentController::getAll'], null, ['GET' => 0], null, false, false, null]],
         '/department/create' => [[['_route' => 'create_department', '_controller' => 'App\\Controller\\Employee\\EmpDepartmentController::create'], null, ['POST' => 0], null, false, false, null]],
-        '/employee/emp/families' => [[['_route' => 'app_employee_emp_families', '_controller' => 'App\\Controller\\Employee\\EmpFamiliesController::index'], null, null, null, false, false, null]],
+        '/employee/family' => [[['_route' => 'get_family', '_controller' => 'App\\Controller\\Employee\\EmpFamiliesController::getAll'], null, ['GET' => 0], null, true, false, null]],
+        '/employee/family/create' => [[['_route' => 'create_family', '_controller' => 'App\\Controller\\Employee\\EmpFamiliesController::create'], null, ['POST' => 0], null, false, false, null]],
         '/employee/position' => [[['_route' => 'get_all_position', '_controller' => 'App\\Controller\\Employee\\EmpPositionsController::getAll'], null, ['GET' => 0], null, true, false, null]],
         '/employee/position/create' => [[['_route' => 'create_positions', '_controller' => 'App\\Controller\\Employee\\EmpPositionsController::Create'], null, ['POST' => 0], null, false, false, null]],
-        '/employee/family/relationships' => [[['_route' => 'app_employee_family_relationships', '_controller' => 'App\\Controller\\Employee\\FamilyRelationshipsController::index'], null, null, null, false, false, null]],
+        '/employee/family/relationship' => [[['_route' => 'get_family_relationships', '_controller' => 'App\\Controller\\Employee\\FamilyRelationshipsController::getAll'], null, ['GET' => 0], null, true, false, null]],
+        '/employee/family/relationship/create' => [[['_route' => 'create_family_relationships', '_controller' => 'App\\Controller\\Employee\\FamilyRelationshipsController::create'], null, ['POST' => 0], null, false, false, null]],
         '/employee/school/degrees' => [[['_route' => 'app_employee_school_degrees', '_controller' => 'App\\Controller\\Employee\\SchoolDegreesController::index'], null, null, null, false, false, null]],
         '/user' => [[['_route' => 'get_user', '_controller' => 'App\\Controller\\Employee\\UserController::getAllUser'], null, ['GET' => 0], null, false, false, null]],
         '/user/create' => [[['_route' => 'create_user', '_controller' => 'App\\Controller\\Employee\\UserController::create'], null, ['POST' => 0], null, false, false, null]],
@@ -44,19 +46,27 @@ return [
                         .'|update/([^/]++)(*:99)'
                         .'|delete/([^/]++)(*:121)'
                     .')'
+                    .'|family/(?'
+                        .'|update/([^/]++)(*:155)'
+                        .'|delete/([^/]++)(*:178)'
+                        .'|relationship/(?'
+                            .'|update/([^/]++)(*:217)'
+                            .'|delete/([^/]++)(*:240)'
+                        .')'
+                    .')'
                     .'|position/(?'
-                        .'|update/([^/]++)(*:157)'
-                        .'|delete/([^/]++)(*:180)'
+                        .'|update/([^/]++)(*:277)'
+                        .'|delete/([^/]++)(*:300)'
                     .')'
                 .')'
                 .'|/department/(?'
-                    .'|update/([^/]++)(*:220)'
-                    .'|delete/([^/]++)(*:243)'
-                    .'|([^/]++)(*:259)'
+                    .'|update/([^/]++)(*:340)'
+                    .'|delete/([^/]++)(*:363)'
+                    .'|([^/]++)(*:379)'
                 .')'
-                .'|/user/update/([^/]++)(*:289)'
-                .'|/api/user/delete/([^/]++)(*:322)'
-                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:358)'
+                .'|/user/update/([^/]++)(*:409)'
+                .'|/api/user/delete/([^/]++)(*:442)'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:478)'
             .')/?$}sDu',
     ],
     [ // $dynamicRoutes
@@ -64,14 +74,18 @@ return [
         76 => [[['_route' => 'delete_bank_account_info', '_controller' => 'App\\Controller\\Employee\\BankAccountsController::delete'], ['id'], ['DELETE' => 0], null, false, true, null]],
         99 => [[['_route' => 'update_banks', '_controller' => 'App\\Controller\\Employee\\BanksController::update'], ['id'], ['POST' => 0], null, false, true, null]],
         121 => [[['_route' => 'delete_banks', '_controller' => 'App\\Controller\\Employee\\BanksController::delete'], ['id'], ['DELETE' => 0], null, false, true, null]],
-        157 => [[['_route' => 'update_position', '_controller' => 'App\\Controller\\Employee\\EmpPositionsController::update'], ['id'], ['POST' => 0], null, false, true, null]],
-        180 => [[['_route' => 'delete_position', '_controller' => 'App\\Controller\\Employee\\EmpPositionsController::delete'], ['id'], ['DELETE' => 0], null, false, true, null]],
-        220 => [[['_route' => 'update_department', '_controller' => 'App\\Controller\\Employee\\EmpDepartmentController::update'], ['id'], ['POST' => 0], null, false, true, null]],
-        243 => [[['_route' => 'delete_department', '_controller' => 'App\\Controller\\Employee\\EmpDepartmentController::delete'], ['id'], ['DELETE' => 0], null, false, true, null]],
-        259 => [[['_route' => 'show_department', '_controller' => 'App\\Controller\\Employee\\EmpDepartmentController::show'], ['id'], ['GET' => 0], null, false, true, null]],
-        289 => [[['_route' => 'update_user', '_controller' => 'App\\Controller\\Employee\\UserController::update'], ['id'], ['POST' => 0], null, false, true, null]],
-        322 => [[['_route' => 'delete_user', '_controller' => 'App\\Controller\\Employee\\UserController::delete'], ['id'], ['DELETE' => 0], null, false, true, null]],
-        358 => [
+        155 => [[['_route' => 'update_family', '_controller' => 'App\\Controller\\Employee\\EmpFamiliesController::update'], ['id'], ['POST' => 0], null, false, true, null]],
+        178 => [[['_route' => 'delete_family', '_controller' => 'App\\Controller\\Employee\\EmpFamiliesController::delete'], ['id'], ['DELETE' => 0], null, false, true, null]],
+        217 => [[['_route' => 'update_family_relationships', '_controller' => 'App\\Controller\\Employee\\FamilyRelationshipsController::update'], ['id'], ['POST' => 0], null, false, true, null]],
+        240 => [[['_route' => 'delete_family_relationships', '_controller' => 'App\\Controller\\Employee\\FamilyRelationshipsController::delete'], ['id'], ['DELETE' => 0], null, false, true, null]],
+        277 => [[['_route' => 'update_position', '_controller' => 'App\\Controller\\Employee\\EmpPositionsController::update'], ['id'], ['POST' => 0], null, false, true, null]],
+        300 => [[['_route' => 'delete_position', '_controller' => 'App\\Controller\\Employee\\EmpPositionsController::delete'], ['id'], ['DELETE' => 0], null, false, true, null]],
+        340 => [[['_route' => 'update_department', '_controller' => 'App\\Controller\\Employee\\EmpDepartmentController::update'], ['id'], ['POST' => 0], null, false, true, null]],
+        363 => [[['_route' => 'delete_department', '_controller' => 'App\\Controller\\Employee\\EmpDepartmentController::delete'], ['id'], ['DELETE' => 0], null, false, true, null]],
+        379 => [[['_route' => 'show_department', '_controller' => 'App\\Controller\\Employee\\EmpDepartmentController::show'], ['id'], ['GET' => 0], null, false, true, null]],
+        409 => [[['_route' => 'update_user', '_controller' => 'App\\Controller\\Employee\\UserController::update'], ['id'], ['POST' => 0], null, false, true, null]],
+        442 => [[['_route' => 'delete_user', '_controller' => 'App\\Controller\\Employee\\UserController::delete'], ['id'], ['DELETE' => 0], null, false, true, null]],
+        478 => [
             [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
             [null, null, null, null, false, false, 0],
         ],

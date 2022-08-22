50baafcd3f38b697732ed41075a9c857d5eaf974
Thorn324 <lychanthorn2000@gmail.com>
2022-08-22 08:46:41 +0700

    update 22 Aug 2022

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -15,7 +15,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MailerController extends AbstractController
 {
-    private $em;
+    private EntityManagerInterface $em;
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->em = $entityManager;
@@ -33,19 +33,50 @@ class MailerController extends AbstractController
         }
 
         $token = $JWTToken->create($user);
+        $htmlAsText = '
+            <html lang="en">
+            <body>
+                <div style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
+                    <div style="width: 660px; box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px; padding: 10px 30px;">
+                        <div style=" display: flex; flex-direction: column; align-items: center;">
+                            <h1 style="font-weight: 900; color: rgb(3, 4, 4);">Password Reset Request</h1>
+                        </div>
+                        <div>
+                            <p>Dear Allweb RMS user,</p>
+                            <p>We have received your request to reset your password. Please click the link below to complete the reset:</p>
+                        </div>
+                        <div style=" display: flex; flex-direction: column; align-items: center;">
+                            ' . '<a href="`http://localhost:4200/password/reset?verify=`' .$token . '">'.'
+                                <button style="padding: 10px 50px; border-radius: 10px; background-color: #ff6c37; color: white;" type="button">
+                                    <h4 style="margin: 0;">Reset My Password</h4>
+                                </button>
+                            </a>
+                        </div>
+                        <div style="display: flex; flex-direction: column; align-items: center; margin-top: 20px;">
+                            <div>
+                                <div>This email was sent to '. $param['email'] .', <span style="color: rgb(245, 77, 31);">which is associated with a Allweb RMS account.</span></div>
+                            </div>
+                        </div>
+                    </div>
+            
+                </div>
+            </body>
+            </html>
+        ';
 
         $email = (new Email())
             ->from('lychanthorn2000@gmail.com')
             ->to($param['email'])
-            ->subject('Forget password!')
-            ->text("Reset password: http://localhost:4200/password/reset?verify=" . $token);
+            ->subject('Reset your Allweb RMS account password')
+            ->html($htmlAsText);
         try {
             $mailer->send($email);
         } catch (TransportExceptionInterface $e) {
             return $this->json(array("success" => false, "message" => $e->getMessage()), 400);
         }
         return $this->json([
-            'message' => 'Email Sending  Test',
-        ]);
+            'success' => true,
+            'message' => 'Email send successfully',
+        ], 200);
     }
 }
--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -6,6 +6,7 @@ use App\Entity\Users;
 use Doctrine\ORM\EntityManagerInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -13,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ResetPasswordController extends AbstractController
 {
-    private $em;
+    private EntityManagerInterface $em;
     public function __construct(EntityManagerInterface $entityManager){
         $this->em = $entityManager;
     }
@@ -26,6 +27,9 @@ class ResetPasswordController extends AbstractController
             $param = json_decode($request->getContent(), true);
             if(isset($param['email'])){
                 $user = $this->em->getRepository(Users::class)->findOneBy(array("email" => $param['email']));
+                if(!$user) {
+                    throw new RuntimeException("User not exist");
+                }
                 $hashedPassword = $passwordHasher->hashPassword($user,$param['password']);
                 $user->setPassword($hashedPassword);
 
